f37e24ce8cfe86c88393a6f07d0b2890f05aa57a
avshvarts <alexandr.shvartsberg@gmail.com>
2016-03-31 16:02:30 +0300

    Added py file for testing

---
+++ FlyElephant_test.py
@@ -0,0 +1,17 @@
+fout = open("FlyElephant_test.txt","w")
+print >> fout, "Test string for FlyElephant"
+print "Check the test file..."
+
+fin = open("FlyElephant_test_input.txt","r")
+for line in fin:
+    print line
+
+import numpy as np
+print np.eye(3)
+
+import pandas as pd
+X = pd.DataFrame({"A":[1,2,3],"Score":[0,5,7]})
+print "\n",X
+
+from sklearn.linear_model import LogisticRegression
+print "sklearn.linear_model.LogisticRegression imported"
